a4eeccf2c9de2dbd19feb0df83f42290e2e7b0bf
hunterdelyx1 <hunterdelyx1@gmail.com>
2015-10-02 16:14:31 +0300

    init

---
+++ src/main/java/ru/spb/herzen/ivt3/App.java
@@ -0,0 +1,13 @@
+package ru.spb.herzen.ivt3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
