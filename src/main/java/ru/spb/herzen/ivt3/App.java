685123146628b4ff7ad79264ee8e8fbfdb4f3b2e
kirpach <knastin@mail.ru>
2015-12-11 13:37:52 +0300

    Base

--- src/main/java/ru/spb/herzen/ivt3/App.java
+++ src/main/java/ru/spb/herzen/ivt3/App.java
@@ -2,22 +2,24 @@ package ru.spb.herzen.ivt3;
 
 import org.kohsuke.randname.RandomNameGenerator;
 
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class App
 {
     public static void main( String[] args )
     {
         RandomNameGenerator rand = new RandomNameGenerator(0); // 0 is random seed
-
-        String[] array = new String[15];
+        List<String> array = new ArrayList<String>();
 
         /* === Exercise 1 === */
         System.out.println("=== Zad 1 ===");
         for(int i=0; i<15; i++){
             String name = rand.next();
-            array[i] = name;
+            array.add(name);
         }
+
         for(String tmp : array){
             System.out.println("Hello, " + tmp);
         }
@@ -25,7 +27,7 @@ public class App
 
         /* === Exercise 2 === */
         System.out.println("=== Zad 2 ===");
-        Arrays.sort(array);
+        Collections.sort(array);
         for(String tmp : array){
             System.out.println("Hello, " + tmp);
         }
@@ -34,69 +36,58 @@ public class App
         /* === Exercise 3 === */
         System.out.println("=== Zad 3 ===");
         for(int i=2; i<5; i++){
-            System.out.println("Hello, " + array[i]);
+            System.out.println("Hello, " + array.get(i));
         }
 
 
         /* === Exercise 4 === */
         System.out.println("=== Zad 4 ===");
         for(int i=0; i<15; i++){
-            array[i] = array[i].toUpperCase();
-        }
-        for(int i=0; i<15; i++){
-            System.out.println("Hello, " + array[i]);
+            array.set(i, array.get(i).toUpperCase());
         }
 
+        for(String tmp : array){
+            System.out.println("Hello, " + tmp);
+        }
 
         /* === Exercise 5 === */
         System.out.println("=== Zad 5 ===");
         for(int i=0; i<15; i++){
-            array[i] = array[i].toUpperCase();
+            array.set(i, array.get(i).toUpperCase());
         }
-        for(int i=0; i<15; i++){
-            System.out.println("Hello, " + array[i]);
+
+        for(String tmp : array){
+            System.out.println("Hello, " + tmp);
         }
 
         /* === Exercise 6 === */
         System.out.println("=== Zad 6 ===");
-        int[] num_array = new int[30];
+        List<Integer> num_array = new ArrayList<Integer>();
 
         for(int i=0; i<15; i++){
-            String name = rand.next();
-            num_array[i] = (int) Math.floor(Math.random()*201);
+            num_array.add((int) Math.floor(Math.random() * 201));
         }
         int count = 0;
-        for(int i=0; i<15; i++){
-            if(num_array[i]%2==0){
+        for(Integer tmp : num_array){
+            if(tmp%2==0){
                 count++;
             }
-            System.out.println(i + " : " + num_array[i]);
+            System.out.println(tmp);
         }
         System.out.println("Amount of even numbers: " + count);
-
 
         /* === Exercise 7 === */
         System.out.println("=== Zad 7 ===");
         String my_name = "Valentin";
-        char[] char_array = my_name.toCharArray();
-        Arrays.sort(char_array);
-        for(char x : char_array){
-            System.out.println(x);
-        }
+        List<Character> char_array = my_name.chars().mapToObj(i -> new Character((char) i)).sorted().collect(Collectors.toList());
+        char_array.forEach(System.out::println);
 
         /* === Exercise 8 === */
         System.out.println("=== Zad 8 ===");
         count = 0;
-        char[] vowels_array= {'A','E','I','O','U','a','e','i','o','u'};
-        char tmp_char='\0';
-        for(char x : char_array){
-            if(Arrays.binarySearch(vowels_array, x)>0){
-                if(x!=tmp_char) {
-                    tmp_char = x;
-                    count++;
-                }
-            }
-        }
-        System.out.println("Amount of different vowels: "+count);
+        String vowels_array= "AEIOUaeiou";
+
+        count = my_name.chars().mapToObj(i -> new Character((char) i)).filter(i -> vowels_array.indexOf(i) != -1).collect(Collectors.toList()).size();
+        System.out.println("Amount of different vowels: " + count);
     }
 }
